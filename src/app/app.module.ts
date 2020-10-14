846b345b811e5a565e5bd2d73ef8a9ee58317bd3
paavankumar@cse.iitb.ac.in <paavanKumar@cse.iitb.ac.in>
2020-10-15 01:10:11 +0530

    51commit1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FormComponent } from './form/form.component';
+import { ContactPageComponent } from './contact-page/contact-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FormComponent,
+    ContactPageComponent
   ],
   imports: [
     BrowserModule,
