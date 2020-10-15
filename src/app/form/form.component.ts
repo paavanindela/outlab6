13a2f85991b9b7f3757b5e6462823b4c256ad2ab
paavankumar@cse.iitb.ac.in <paavanKumar@cse.iitb.ac.in>
2020-10-15 14:46:06 +0530

    51commit2

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.scss']
 })
+
 export class FormComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  frgrp = new FormGroup({
+  	name: new FormControl('',Validators.required),
+  	email: new FormControl('',Validators.required),
+  	feedback: new FormControl('',Validators.required),
+	comment: new FormControl('')
+  });
+  
+  onSubmit() {
+	console.warn(this.frgrp.value);
+  }
 }
